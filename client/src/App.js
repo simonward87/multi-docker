7c9376392c3b29657b0c6a6767e8d3ea2ceffab8
Simon Ward <mr.nodi@gmail.com>
2020-10-08 10:29:33 +0100

    updated files to match tutorial files

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,29 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import OtherPage from './OtherPage'
-import Fib from './Fib'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Fib Calculator</h1>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Fib Calculator version 2</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
           <div>
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
           </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </Router>
-  )
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App
+export default App;
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,54 +1,55 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: '',
-  }
+    index: ''
+  };
 
   componentDidMount() {
-    this.fetchValues()
-    this.fetchIndexes()
+    this.fetchValues();
+    this.fetchIndexes();
   }
 
   async fetchValues() {
-    const { data } = await axios.get('/api/values/current')
-    this.setState({ values: data })
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const { data } = await axios.get('/api/values/all')
-    this.setState({ seenIndexes: data })
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
   }
 
-  handleSubmit = async (e) => {
-    e.preventDefault()
+  handleSubmit = async event => {
+    event.preventDefault();
 
     await axios.post('/api/values', {
-      index: this.state.index,
-    })
-    this.setState({ index: '' })
-  }
+      index: this.state.index
+    });
+    this.setState({ index: '' });
+  };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ')
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
-    console.log('this.state.values = ', this.state.values)
-    const entries = []
+    const entries = [];
 
     for (let key in this.state.values) {
       entries.push(
         <div key={key}>
-          For index {key}, I calculated {this.state.values[key]}
+          For index {key} I calculated {this.state.values[key]}
         </div>
-      )
+      );
     }
 
-    return entries
+    return entries;
   }
 
   render() {
@@ -58,18 +59,19 @@ class Fib extends Component {
           <label>Enter your index:</label>
           <input
             value={this.state.index}
-            onChange={(e) => this.setState({ index: e.target.value })}
+            onChange={event => this.setState({ index: event.target.value })}
           />
           <button>Submit</button>
         </form>
 
         <h3>Indexes I have seen:</h3>
         {this.renderSeenIndexes()}
+
         <h3>Calculated Values:</h3>
         {this.renderValues()}
       </div>
-    )
+    );
   }
 }
 
-export default Fib
+export default Fib;
